61e5c00c4b2d8be48918ac549065338ea5ac53b1
Earl Miles <merlin@logrus.com>
2007-02-04 06:15:49 +0000

    jquery queue manager

---
+++ nodequeue.js
@@ -0,0 +1,100 @@
+// $Id$
+
+Drupal.nodequeue = {};
+
+Drupal.nodequeue.autoAttach = function() {
+  $('.nodequeue-js-hide').show();
+  $('a.nodequeue-move-up').click(function(e) {
+    var item = $(this).parent().parent();
+    var prev = $(this).parent().parent().prev();
+    if (item.attr('id') != prev.attr('id')) {
+      // swap classes to retain striping.
+      var temp = item.attr('class');
+      item.attr('class', prev.attr('class'));
+      prev.attr('class', temp);
+
+      // move item
+      prev.before(item);
+    }
+    return false;
+  });
+
+  $('a.nodequeue-move-down').click(function() {
+    var item = $(this).parent().parent();
+    var next = $(this).parent().parent().next();
+    if (item.attr('id') != next.attr('id')) {
+      // swap classes to retain striping.
+      var temp = item.attr('class');
+      item.attr('class', next.attr('class'));
+      next.attr('class', temp);
+
+      // move item
+      next.after(item);
+    }
+    return false;
+  });
+
+  $('a.nodequeue-move-front').click(function() {
+    var current = $(this).parent().parent();
+    var item = current;
+    var prev = $(this).parent().parent().prev();
+    while (current.attr('id') != prev.attr('id')) {
+      prev.toggleClass('even').toggleClass('odd');
+      current = prev;
+      prev = $('#' + prev.attr('id')).prev();
+    }
+    if (item.attr('id') != current.attr('id')) {
+      item.removeClass('even').addClass('odd');
+      current.before(item);
+    }
+    return false;
+  });
+
+  $('a.nodequeue-move-back').click(function() {
+    var current = $(this).parent().parent();
+    var item = current;
+    var next = $(this).parent().parent().next();
+    var count = 0;
+    while (current.attr('id') != next.attr('id')) {
+      count++;
+      next.toggleClass('even').toggleClass('odd');
+      current = next;
+      next = $('#' + next.attr('id')).next();
+    }
+    if (item.attr('id') != current.attr('id')) {
+      if (count % 2 == 1) {
+        item.toggleClass('even').toggleClass('odd');
+      }
+      current.after(item);
+    }
+    return false;
+  });
+
+  $('a.nodequeue-remove').click(function() {
+    var current = $(this).parent().parent();
+    var next = $(this).parent().parent().next();
+    while (current.attr('id') != next.attr('id')) {
+      next.toggleClass('even').toggleClass('odd');
+      current = next;
+      next = $('#' + next.attr('id')).next();
+    }
+
+    $(this).parent().parent().remove();    
+    return false;
+  });
+
+  $('input.nodequeue-save').click(function() {
+    var order = '';
+    $('#nodequeue-table tr.nodequeue-row').each(function() {
+      if (order != '') {
+        order += ',';
+      }
+      order += $(this).attr('id').replace('nodequeue-row-', '');
+    });
+    $('#nodequeue-order').val(order);
+  });
+}
+
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.nodequeue.autoAttach);
+}
