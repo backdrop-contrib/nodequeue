db730fbc827b6ea62fd22daa7fa04ed061f5d86e
Earl Miles <merlin@logrus.com>
2011-04-20 10:45:07 -0700

    Issue #994996 by slashrsm: Update javascript for D7 behaviors.

--- nodequeue_dragdrop.js
+++ nodequeue_dragdrop.js
@@ -1,98 +1,109 @@
+(function ($) {
 
-Drupal.behaviors.nodequeueDrag = function(context) {
-  var tableDrag = Drupal.tableDrag['nodequeue-dragdrop'];
+Drupal.behaviors.nodequeueDrag = {
+  attach: function(context) {
+    var tableDrag = Drupal.tableDrag['nodequeue-dragdrop'];
 
-  tableDrag.onDrop = function() {
-    $('td.position').each(function(i){
-      $(this).html(i + 1);
+    tableDrag.onDrop = function() {
+      $('td.position').each(function(i){
+        $(this).html(i + 1);
+      });
+    }
+  }
+};
+
+Drupal.behaviors.nodequeueReverse = {
+  attach: function(context) {
+    $('#edit-reverse').click(function(){
+      // reverse table rows...
+      $('tr.draggable').each(function(i){
+        $('.nodequeue-dragdrop tbody').prepend(this);
+      });
+
+      // ...and update node positions
+      var size = $('.node-position').size();
+      $('.node-position').each(function(i){
+        var val = $(this).val();
+        $(this).val(size - val + 1);
+      });
+
+      nodequeueInsertChangedWarning();
+      nodequeueRestripeTable();
+
+      return false;
     });
   }
-}
-
-Drupal.behaviors.nodequeueReverse = function(context) {
-  $('#edit-reverse').click(function(){
-    // reverse table rows...
-    $('tr.draggable').each(function(i){
-      $('.nodequeue-dragdrop tbody').prepend(this);
-    });
-
-    // ...and update node positions
-    var size = $('.node-position').size();
-    $('.node-position').each(function(i){
-      var val = $(this).val();
-      $(this).val(size - val + 1);
-    });
-
-    nodequeueInsertChangedWarning();
-    nodequeueRestripeTable();
-
-    return false;
-  });
 };
 
-Drupal.behaviors.nodequeueShuffle = function(context) {
-  $('#edit-shuffle').click(function(){
-    // randomize table rows...
-    var rows = $('table.nodequeue-dragdrop tbody tr:not(:hidden)').get();
-    rows.sort(function(){return (Math.round(Math.random())-0.5);});
-    $.each(rows, function(i, row) {
-      $('.nodequeue-dragdrop tbody').prepend(this);
+Drupal.behaviors.nodequeueShuffle = {
+  attach: function(context) {
+    $('#edit-shuffle').click(function(){
+      // randomize table rows...
+      var rows = $('table.nodequeue-dragdrop tbody tr:not(:hidden)').get();
+      rows.sort(function(){return (Math.round(Math.random())-0.5);});
+      $.each(rows, function(i, row) {
+        $('.nodequeue-dragdrop tbody').prepend(this);
+      });
+
+      var reverse = Drupal.settings.nodequeue.reverse;
+
+      // ...and update node positions
+      var size = reverse ? $('.node-position').size() : 1;
+      $('.node-position').each(function(i){
+        var val = $(this).val();
+        $(this).val(size);
+        reverse ? size-- : size++;
+      });
+
+      nodequeueInsertChangedWarning();
+      nodequeueRestripeTable();
+
+      return false;
     });
-
-    var reverse = Drupal.settings.nodequeue.reverse;
- 
-    // ...and update node positions
-    var size = reverse ? $('.node-position').size() : 1;
-    $('.node-position').each(function(i){
-      var val = $(this).val();
-      $(this).val(size);
-      reverse ? size-- : size++;
-    });
-
-    nodequeueInsertChangedWarning();
-    nodequeueRestripeTable();
-
-    return false;
-  });
+  }
 };
 
-Drupal.behaviors.nodequeueClear = function(context) {
-  $('#edit-clear').click(function(){
-    // mark nodes for removal
-    $('.node-position').each(function(i){
-      $(this).val('r');
+Drupal.behaviors.nodequeueClear = {
+  attach: function(context) {
+    $('#edit-clear').click(function(){
+      // mark nodes for removal
+      $('.node-position').each(function(i){
+        $(this).val('r');
+      });
+
+      // remove table rows...
+      rows = $('table.nodequeue-dragdrop tbody tr:not(:hidden)').hide();
+
+      nodequeuePrependEmptyMessage();
+      nodequeueInsertChangedWarning();
+
+      return false;
     });
-
-    // remove table rows...
-    rows = $('table.nodequeue-dragdrop tbody tr:not(:hidden)').hide();
-
-    nodequeuePrependEmptyMessage();
-    nodequeueInsertChangedWarning();
-
-    return false;
-  });
+  }
 };
 
-Drupal.behaviors.nodequeueRemoveNode = function(context) {
-  $('a.nodequeue-remove').css('display', 'block');
-  $('a.nodequeue-remove').click(function() {
-    a = $(this).attr('id');
-    a = '#' + a.replace('nodequeue-remove-', 'edit-') + '-position';
-    $(a).val('r');
+Drupal.behaviors.nodequeueRemoveNode = {
+  attach: function(context) {
+    $('a.nodequeue-remove').css('display', 'block');
+    $('a.nodequeue-remove').click(function() {
+      a = $(this).attr('id');
+      a = '#' + a.replace('nodequeue-remove-', 'edit-') + '-position';
+      $(a).val('r');
 
-    // hide the current row
-    $(this).parent().parent().fadeOut('fast', function(){
-      if ($('table.nodequeue-dragdrop tbody tr:not(:hidden)').size() == 0) {
-        nodequeuePrependEmptyMessage();
-      }
-      else {
-        nodequeueRestripeTable()
-        nodequeueInsertChangedWarning();
-      }
+      // hide the current row
+      $(this).parent().parent().fadeOut('fast', function(){
+        if ($('table.nodequeue-dragdrop tbody tr:not(:hidden)').size() == 0) {
+          nodequeuePrependEmptyMessage();
+        }
+        else {
+          nodequeueRestripeTable()
+          nodequeueInsertChangedWarning();
+        }
+      });
+
+      return false;
     });
-
-    return false;
-  });
+  }
 }
 
 Drupal.behaviors.nodequeueClearTitle = function(context) {
@@ -143,3 +154,5 @@ function nodequeueInsertChangedWarning() {
     Drupal.tableDrag['nodequeue-dragdrop'].changed = true;
   }
 }
+
+})(jQuery);
